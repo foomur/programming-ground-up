a8f2d92f76f0ca317f685b28ea741a47cf6ea59d
foomur <35081404+foomur@users.noreply.github.com>
2018-01-27 20:46:11 +0000

    Update power.s

--- chapter 4/power.s
+++ chapter 4/power.s
@@ -50,28 +50,28 @@ int $0x80
 
 .type power, @function
 power:
-	pushl %ebp 			  #save old base pointer
-	movl %esp, %ebp 		  #make stack pointer the base pointer
-	subl $4, %esp 			  #get room for our local storage
-	movl 8(%ebp), %ebx 		  #put first argument in %eax
-	movl 12(%ebp), %ecx 		  #put second argument in %ecx
-	movl %ebx, -4(%ebp) 		  #store current result
+	pushl %ebp 			  # save old base pointer
+	movl %esp, %ebp 		  # make stack pointer the base pointer
+	subl $4, %esp 			  # get room for our local storage
+	movl 8(%ebp), %ebx 		  # put first argument in %eax
+	movl 12(%ebp), %ecx 		  # put second argument in %ecx
+	movl %ebx, -4(%ebp) 		  # store current result
 	
 power_loop_start:
 	
-	cmpl $1, %ecx 			  #if the power is 1, we are done
+	cmpl $1, %ecx 			  # if the power is 1, we are done
 	je end_power
-	movl -4(%ebp), %eax 		  #move the current result into %eax
-	imull %ebx, %eax 		  #multiply the current result by
-					  #the base number
-	movl %eax, -4(%ebp) 	          #store the current result
-	decl %ecx 		          #decrease the power
-	jmp power_loop_start 	          #run for the next power
+	movl -4(%ebp), %eax 		  # move the current result into %eax
+	imull %ebx, %eax 		  # multiply the current result by
+					  # the base number
+	movl %eax, -4(%ebp) 	          # store the current result
+	decl %ecx 		          # decrease the power
+	jmp power_loop_start 	          # run for the next power
 	
 end_power:
-	movl -4(%ebp), %eax 	          #return value goes in %eax
-	movl %ebp, %esp 		  #restore the stack pointer
-	popl %ebp 			  #restore the base pointer
+	movl -4(%ebp), %eax 	          # return value goes in %eax
+	movl %ebp, %esp 		  # restore the stack pointer
+	popl %ebp 			  # restore the base pointer
 ret
 
 
